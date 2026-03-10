0a5a27c893295fc53e1daba90de05169793d9ec9
Javier Puche <javierpuchevictoria@gmail.com>
2026-03-10 10:48:42 +0100

    fix(nuxt): ensure valiform plugin runs after i18n (#14)

--- nuxt/runtime/plugin.ts
+++ nuxt/runtime/plugin.ts
@@ -3,26 +3,29 @@ import { FormsPlugin, registerRules } from '@overgaming/valiform';
 // @ts-ignore virtual module resolved by Nuxt at build time
 import { pluginOptions, valiformConfig } from '#build/valiform/options.mjs';
 
-export default defineNuxtPlugin((nuxtApp) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { syncI18n, ...formsOpts } = pluginOptions;
-  const opts = { ...formsOpts };
+export default defineNuxtPlugin({
+  enforce: 'post',
+  setup(nuxtApp) {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { syncI18n, ...formsOpts } = pluginOptions;
+    const opts = { ...formsOpts };
 
-  if (valiformConfig?.locales) {
-    opts.locales = valiformConfig.locales;
-  }
-
-  if (syncI18n) {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const i18nLocale = (nuxtApp as any).$i18n?.locale;
-    if (i18nLocale) {
-      opts.locale = i18nLocale;
+    if (valiformConfig?.locales) {
+      opts.locales = valiformConfig.locales;
     }
-  }
 
-  nuxtApp.vueApp.use(FormsPlugin, opts);
+    if (syncI18n) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const i18nLocale = (nuxtApp as any).$i18n?.locale;
+      if (i18nLocale) {
+        opts.locale = i18nLocale;
+      }
+    }
 
-  if (valiformConfig?.rules) {
-    registerRules(valiformConfig.rules);
+    nuxtApp.vueApp.use(FormsPlugin, opts);
+
+    if (valiformConfig?.rules) {
+      registerRules(valiformConfig.rules);
+    }
   }
 });
